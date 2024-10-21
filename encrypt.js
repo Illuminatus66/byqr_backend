8c5a6f9bd285aa729492c6eaf358cbd7b87175ed
Illuminatus66 <varunsodhani123@gmail.com>
2024-10-22 00:49:14 +0530

    final commit for backend

---
+++ encrypt.js
@@ -0,0 +1,22 @@
+//Generate encrypted password for your admin so that you can set it
+//in a manually/programatically created admin user's password field.
+//Remember to never store passwords in plaintext, be it for your user or your admin
+
+const bcrypt = require('bcryptjs');
+
+async function encryptPassword(password) {
+  try {
+    const saltRounds = 12;  // Set salt rounds to whatever you want
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log('Encrypted Password:', hashedPassword);
+  } catch (error) {
+    console.error('Error encrypting password:', error);
+  }
+}
+
+//Replace 'yourpasswordhere' with the password you want to encrypt
+const password = 'yourpasswordhere';
+
+//In the root of the directory run node encrypt.js in the command
+//line to generate your encrypted password
+encryptPassword(password);
